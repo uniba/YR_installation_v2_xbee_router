200a9b020a1296644262133e2f309a4a707843b9
hideyukisaito <heeeeeemail@gmail.com>
2012-03-09 16:50:59 +0900

    とりあえず

--- YR_installation_v2_xbee_router.ino
+++ YR_installation_v2_xbee_router.ino
@@ -68,10 +68,17 @@ void loop()
     if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
     {
       xbee.getResponse().getZBRxResponse(zbRes);
+      /*
       uint8_t devices = zbRes.getData(3);
       uint16_t targets = (zbRes.getData(4) << 8) + zbRes.getData(5);
       uint16_t volume = (zbRes.getData(6) << 8) + zbRes.getData(7);
       uint16_t time = (zbRes.getData(8) << 8) + zbRes.getData(9);
+      */
+      uint8_t devices = zbRes.getData(5);
+      uint16_t targets = (zbRes.getData(8) << 8) + zbRes.getData(9);
+      uint16_t volume = (zbRes.getData(10) << 8) + zbRes.getData(11);
+      uint16_t time = (zbRes.getData(12) << 8) + zbRes.getData(13);
+      
 
       // Napion の信号を無視する時間をセット
       //TODO : napion と 音楽の優先順位をコマンド切り替え可能にする
@@ -82,29 +89,7 @@ void loop()
     }
   }
 
-  // TODO: val が上下動するのでまるめる(中央値もしくは平均値)
-  // 現在は直前より400以上変動した時を拾っている
-  val = analogRead(napion);
-  if ((abs(prevVal - val) > 400) && (loopStart - xbeeKick > napIgnore))  {
-    if (val > prevVal) {
-      fadeAll(65535, 1024, 3000);
-    }/* else {
-      fadeAll(65535, 1, 3000);
-    }*/
-
-    uint8_t payload[2];
-    payload[0] = val >> 8;
-    payload[1] = val & 255;
-
-    ZBTxRequest zbTx = ZBTxRequest(remoteAddress, payload, sizeof(payload));
-    /* for Debuggin'
-    uint8_t text[] = {'H', 'e', 'l', 'l', 'o'};
-    ZBTxRequest zbTx = ZBTxRequest(remoteAddress, text, sizeof(text));
-    */
-    xbee.send(zbTx);
-    delay(10);
-  }
-  prevVal = val;
+  
 
   tlc_updateFades(loopStart);
 }
@@ -117,7 +102,7 @@ void fade(TLC_CHANNEL_TYPE channel, uint16_t current, uint16_t value, uint16_t t
     tlc_addFade(channel, current, value, startMillis, endMillis);
 }
 
-void fadeAll(uint16_t targets, uint16_t value, uint16_t time) {
+void fadeAll(uint32_t targets, uint16_t value, uint16_t time) {
   for (int i = 0; i < LIGHTS_MAX; i++) {
     if ((targets & (1 << i)) != 0) {
       fade(i, tlc_getCurrentValue(i), value, time);
